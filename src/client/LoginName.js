5bc9ee3a61e3e048950cc6788ee0e29ef0243f3e
daishi <daishi@axlight.com>
2018-03-27 06:57:01 +0900

    use Query

--- src/client/LoginName.js
+++ src/client/LoginName.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 import { CircularProgress } from 'material-ui/Progress';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
-
-const LoginName = ({ height, data: { loading, error, viewer } }) => {
-  if (loading) {
-    return <CircularProgress color="secondary" />;
-  }
-  if (error) {
-    return (
-      <a href="/auth/github">
-        <Button>Log in with GitHub</Button>
-      </a>
-    );
-  }
-  return (
-    <Typography
-      variant="body2"
-      style={{ display: 'inline-flex', alignItems: 'center' }}
-    >
-      <img
-        src={viewer.avatarUrl}
-        alt="icon"
-        style={{ height, borderRadius: 3, marginRight: 5 }}
-      />
-      {viewer.name}
-    </Typography>
-  );
-};
 
 export const QUERY_LOGIN_INFO = gql`
 {
@@ -42,5 +16,34 @@ export const QUERY_LOGIN_INFO = gql`
 }
 `;
 
-const withQuery = graphql(QUERY_LOGIN_INFO);
-export default withQuery(LoginName);
+const LoginName = ({ height }) => (
+  <Query query={QUERY_LOGIN_INFO}>
+    {({ loading, error, data }) => {
+      if (loading) {
+        return <CircularProgress color="secondary" />;
+      }
+      if (error) {
+        return (
+          <a href="/auth/github">
+            <Button>Log in with GitHub</Button>
+          </a>
+        );
+      }
+      return (
+        <Typography
+          variant="body2"
+          style={{ display: 'inline-flex', alignItems: 'center' }}
+        >
+          <img
+            src={data.viewer.avatarUrl}
+            alt="icon"
+            style={{ height, borderRadius: 3, marginRight: 5 }}
+          />
+          {data.viewer.name}
+        </Typography>
+      );
+    }}
+  </Query>
+);
+
+export default LoginName;
