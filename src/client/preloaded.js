bfdf8141d591de8419e0bb5d4c7125aeef35a7f3
daishi <daishi@axlight.com>
2018-07-17 22:04:50 +0900

    update preloaded

--- src/client/preloaded.js
+++ src/client/preloaded.js
@@ -1,118 +1,17 @@
 /* eslint import/prefer-default-export: 0 */
 
-export const ITEMS = [
-  {
-    owner: {
-      login: 'LimeGreenJS',
-      avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4',
-      __typename: 'Organization',
-    },
-    name: 'simple-blog',
-    description: 'LimeGreenJS-enabled simple blog example',
-    url: 'https://github.com/LimeGreenJS/simple-blog',
-    ref: {
-      target: {
-        oid: 'e89be1d2157495549287b1ef35f941dc54814262',
-        __typename: 'Commit',
-      },
-      __typename: 'Ref',
-    },
-    stargazers: { totalCount: 3, __typename: 'StargazerConnection' },
-    __typename: 'Repository',
-  },
-  {
-    owner: {
-      login: 'LimeGreenJS',
-      avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4',
-      __typename: 'Organization',
-    },
-    name: 'simple-todos',
-    description: 'LimeGreenJS-enabled simple todos example',
-    url: 'https://github.com/LimeGreenJS/simple-todos',
-    ref: {
-      target: {
-        oid: 'af481dc440194d98a0b07706a3a82847263a06c8',
-        __typename: 'Commit',
-      },
-      __typename: 'Ref',
-    },
-    stargazers: { totalCount: 1, __typename: 'StargazerConnection' },
-    __typename: 'Repository',
-  },
-  {
-    owner: {
-      login: 'LimeGreenJS',
-      avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4',
-      __typename: 'Organization',
-    },
-    name: 'dynamic-import',
-    description: 'LimeGreenJS-enabled dynamic import example',
-    url: 'https://github.com/LimeGreenJS/dynamic-import',
-    ref: {
-      target: {
-        oid: '4795c402ad7803a7b43f25c397443554988ea8da',
-        __typename: 'Commit',
-      },
-      __typename: 'Ref',
-    },
-    stargazers: { totalCount: 1, __typename: 'StargazerConnection' },
-    __typename: 'Repository',
-  },
-  {
-    owner: {
-      login: 'LimeGreenJS',
-      avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4',
-      __typename: 'Organization',
-    },
-    name: 'hello-world',
-    description: 'LimeGreenJS-enabled hello world example',
-    url: 'https://github.com/LimeGreenJS/hello-world',
-    ref: {
-      target: {
-        oid: '2fc8e406d5522a62f7f66bac83001e95d2606df9',
-        __typename: 'Commit',
-      },
-      __typename: 'Ref',
-    },
-    stargazers: { totalCount: 1, __typename: 'StargazerConnection' },
-    __typename: 'Repository',
-  },
-  {
-    owner: {
-      login: 'LimeGreenJS',
-      avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4',
-      __typename: 'Organization',
-    },
-    name: 'standard-todos',
-    description: 'LimeGreenJS-enabled standard todos example',
-    url: 'https://github.com/LimeGreenJS/standard-todos',
-    ref: {
-      target: {
-        oid: '733ae9d5b5fbd67359a8240b25b4bcbe546e469e',
-        __typename: 'Commit',
-      },
-      __typename: 'Ref',
-    },
-    stargazers: { totalCount: 1, __typename: 'StargazerConnection' },
-    __typename: 'Repository',
-  },
-  {
-    owner: {
-      login: 'LimeGreenJS',
-      avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4',
-      __typename: 'Organization',
-    },
-    name: 'hot-module-replacement',
-    description: 'LimeGreenJS-enabled hot module replacement example',
-    url: 'https://github.com/LimeGreenJS/hot-module-replacement',
-    ref: {
-      target: {
-        oid: '0f9ac397c3485aacd3dbe1a9f95c42268ef17a35',
-        __typename: 'Commit',
-      },
-      __typename: 'Ref',
-    },
-    stargazers: { totalCount: 0, __typename: 'StargazerConnection' },
-    __typename: 'Repository',
-  },
-];
+export const ITEMS = [{
+  owner: { login: 'LimeGreenJS', avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4', __typename: 'Organization' }, name: 'simple-blog', description: 'LimeGreenJS-enabled simple blog example', url: 'https://github.com/LimeGreenJS/simple-blog', ref: { target: { oid: 'e89be1d2157495549287b1ef35f941dc54814262', __typename: 'Commit' }, __typename: 'Ref' }, stargazers: { totalCount: 2, __typename: 'StargazerConnection' }, __typename: 'Repository',
+}, {
+  owner: { login: 'LimeGreenJS', avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4', __typename: 'Organization' }, name: 'hello-world', description: 'LimeGreenJS-enabled hello world example', url: 'https://github.com/LimeGreenJS/hello-world', ref: { target: { oid: '2fc8e406d5522a62f7f66bac83001e95d2606df9', __typename: 'Commit' }, __typename: 'Ref' }, stargazers: { totalCount: 1, __typename: 'StargazerConnection' }, __typename: 'Repository',
+}, {
+  owner: { login: 'LimeGreenJS', avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4', __typename: 'Organization' }, name: 'simple-todos', description: 'LimeGreenJS-enabled simple todos example', url: 'https://github.com/LimeGreenJS/simple-todos', ref: { target: { oid: '26063d4d55c4fd155c7a6970308c8e347cb52c45', __typename: 'Commit' }, __typename: 'Ref' }, stargazers: { totalCount: 1, __typename: 'StargazerConnection' }, __typename: 'Repository',
+}, {
+  owner: { login: 'LimeGreenJS', avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4', __typename: 'Organization' }, name: 'dynamic-import', description: 'LimeGreenJS-enabled dynamic import example', url: 'https://github.com/LimeGreenJS/dynamic-import', ref: { target: { oid: '4795c402ad7803a7b43f25c397443554988ea8da', __typename: 'Commit' }, __typename: 'Ref' }, stargazers: { totalCount: 1, __typename: 'StargazerConnection' }, __typename: 'Repository',
+}, {
+  owner: { login: 'LimeGreenJS', avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4', __typename: 'Organization' }, name: 'standard-todos', description: 'LimeGreenJS-enabled standard todos example', url: 'https://github.com/LimeGreenJS/standard-todos', ref: { target: { oid: '4a228eece7d4edc54873c2e8149e83cb78d20688', __typename: 'Commit' }, __typename: 'Ref' }, stargazers: { totalCount: 1, __typename: 'StargazerConnection' }, __typename: 'Repository',
+}, {
+  owner: { login: 'LimeGreenJS', avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4', __typename: 'Organization' }, name: 'hot-module-replacement', description: 'LimeGreenJS-enabled hot module replacement example', url: 'https://github.com/LimeGreenJS/hot-module-replacement', ref: { target: { oid: '0f9ac397c3485aacd3dbe1a9f95c42268ef17a35', __typename: 'Commit' }, __typename: 'Ref' }, stargazers: { totalCount: 0, __typename: 'StargazerConnection' }, __typename: 'Repository',
+}, {
+  owner: { login: 'LimeGreenJS', avatarUrl: 'https://avatars3.githubusercontent.com/u/32977521?v=4', __typename: 'Organization' }, name: 'simple-chat', description: 'LimeGreenJS-enabled simple chat example ', url: 'https://github.com/LimeGreenJS/simple-chat', ref: { target: { oid: 'c4b318b99d39314e98b2a2a423485868bedf3b06', __typename: 'Commit' }, __typename: 'Ref' }, stargazers: { totalCount: 0, __typename: 'StargazerConnection' }, __typename: 'Repository',
+}];
